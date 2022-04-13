6f5a5898704f2d3f61fbfc1e5626412de17a30c0
Shepherd <shepherd.munemo@outlook.com>
2022-04-13 22:06:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//An access modifier in C# specifies who can see and use a resource. 
+//You probably are familiar with public and private. 
+//We use those a lot but did you know that there are others? 
+//In this lesson we are going to cover six different access modifiers and when you would use each. 
+//We are also going to discuss why we have various modifiers and it isn't just about security.
